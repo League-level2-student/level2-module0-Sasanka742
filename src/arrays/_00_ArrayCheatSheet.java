a44da37e1f8a6fa6522aa53b6dc7cba8558830de
League Student <league@iMac-13.attlocal.net>
2019-09-15 11:29:27 -0700

    arryaCheatUnfinished

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,64 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
+	static int smallest;
+	static int largest;
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
+	//1. make an array of 5 Strings
 		
-		//2. print the third element in the array
+		String[] list = {"first","sec","third","fourth", "fifth"};
 		
-		//3. set the third element to a different value
+	//2. print the third element in the array
+		System.out.println(list[2]);
 		
-		//4. print the third element again
+	//3. set the third element to a different value
+		list[2]="word";
+	//4. print the third element again
+		System.out.println(list[2]);
+	//5. use a for loop to print all the values in the array
+		for(int i=0;i<list.length;i++) {
+			System.out.println(list[i]);
+		}
+	//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		
-		//5. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		
+	//6. make an array of 50 integers
+		int[] integers = new int[50];
+ 	//7. use a for loop to make every value of the integer array a random number
+		for(int i=0;i<integers.length;i++) {
+			integers[i] = new Random().nextInt(50);
+		}
+	//8. without printing the entire array, print only the smallest number in the array
+		for(int i=0;i<integers.length;i++) {
+			int temp = integers[i];
+			if(i<=48) {
+				if(integers[i]<integers[i+1]) {
+					if(temp<integers[i+1]) {
+					 temp = integers[i];
+					}
+				}else {
+					temp = integers[i+1];
+				}
+				smallest = temp;
+			}
 		
-		//6. make an array of 50 integers
-		
-		//7. use a for loop to make every value of the integer array a random number
-		
-		//8. without printing the entire array, print only the smallest number in the array
-		
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
+		}
+		System.out.println(smallest);
+	//9 print the entire array to see if step 8 was correct
+		for(int i=0;i<integers.length;i++) {
+			System.out.print(integers[i]+",");
+		}
+	//10. print the largest number in the array.
+		for(int i=0;i<integers.length;i++) {
+			if(i<49) {
+				if(integers[i]>integers[i+1]) {
+					 largest = integers[i];
+				}
+			}
+		}
+		System.out.println("");
+		System.out.println(largest);
 	}
 }
